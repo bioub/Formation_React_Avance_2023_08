87b670cea807b41f78a875f217657160750044ee
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-08-23 15:39:21 +0100

    Sans Redux Persist

--- react-pokemons-app/src/app/app.tsx
+++ react-pokemons-app/src/app/app.tsx
@@ -9,62 +9,51 @@ import PrivateRoute from './private-route';
 import PokemonCompare from './pages/pokemon-compare';
 import Hello from './hoc-render-prop/Hello';
 import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { pokemonsReducer, State } from './store/reducers';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import { PersistGate } from 'redux-persist/integration/react';
-
-import React from 'react';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(
-  persistConfig,
-  combineReducers({
-    pokemons: pokemonsReducer,
-  })
-);
+import { throttle } from 'lodash';
 
 const store = configureStore<State>({
-  reducer: persistedReducer as any,
+  reducer: {
+    pokemons: pokemonsReducer,
+  },
+  preloadedState: JSON.parse(localStorage.getItem('previousState') ?? 'null') ?? undefined,
 });
 
-let persistor = persistStore(store);
+store.subscribe(
+  throttle(() => {
+    localStorage.setItem('previousState', JSON.stringify(store.getState()));
+  }, 1000)
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <div>
-            <nav>
-              <div className="nav-wrapper teal">
-                <Link to="/" className="brand-logo center">
-                  {/* <PassHello>
+        <div>
+          <nav>
+            <div className="nav-wrapper teal">
+              <Link to="/" className="brand-logo center">
+                {/* <PassHello>
                 {(hello: any) => <Hello name="Pokedex" hello={hello} />}
               </PassHello> */}
-                  <Hello name="Pokedex" />
-                </Link>
-              </div>
-            </nav>
-            <Routes>
-              <Route index path="/" element={<PokemonsList />} />
-              <Route path="/login" element={<Login />} />
-              <Route element={<PrivateRoute />}>
-                <Route path="/pokemons" element={<PokemonsList />} />
-                <Route path="/pokemon/add" element={<PokemonAdd />} />
-                <Route path="/pokemons/edit/:id" element={<PokemonEdit />} />
-                <Route path="/pokemons/compare" element={<PokemonCompare />} />
-                <Route path="/pokemons/:id" element={<PokemonsDetail />} />
-              </Route>
-              <Route element={<PageNotFound />} />
-            </Routes>
-          </div>
-        </PersistGate>
+                <Hello name="Pokedex" />
+              </Link>
+            </div>
+          </nav>
+          <Routes>
+            <Route index path="/" element={<PokemonsList />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/pokemons" element={<PokemonsList />} />
+              <Route path="/pokemon/add" element={<PokemonAdd />} />
+              <Route path="/pokemons/edit/:id" element={<PokemonEdit />} />
+              <Route path="/pokemons/compare" element={<PokemonCompare />} />
+              <Route path="/pokemons/:id" element={<PokemonsDetail />} />
+            </Route>
+            <Route element={<PageNotFound />} />
+          </Routes>
+        </div>
       </Provider>
     </BrowserRouter>
   );
